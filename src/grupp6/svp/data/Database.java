d72c9e5346c45d82a78f8472d1b7a9d1f6cf994d
linnpetterssonn <linnlkp@telia.com>
2017-05-03 09:11:57 +0200

    lagt till jsp-filer och servlets

---
+++ src/grupp6/svp/data/Database.java
@@ -0,0 +1,7 @@
+package grupp6.svp.data;
+
+/**
+ * Created by LinnPettersson on 2017-05-03.
+ */
+public class Database {
+}
---
+++ src/grupp6/svp/web/servlet/AboutServlet.java
@@ -0,0 +1,7 @@
+package grupp6.svp.web.servlet;
+
+/**
+ * Created by LinnPettersson on 2017-05-03.
+ */
+public class AboutServlet {
+}
