aa039be5c574477a0a3f425a31ff11f9b094f4e8
alomi136 <alomi136@gmail.com>
2017-05-08 11:16:13 +0200

    Lagt till DTO-paker med DTOS

---
+++ src/grupp6/svp/data/DataTransferObjects/DataTransferObject.java
@@ -0,0 +1,17 @@
+package grupp6.svp.data.DataTransferObjects;
+
+import sun.security.util.ObjectIdentifier;
+
+/**
+ * Created by alo on 2017-05-08.
+ */
+public abstract class DataTransferObject {
+
+    //Objectidentifier id;
+
+    public void save(){};
+
+    public void reload(){};
+
+    public void delete(){};
+}
---
+++ src/grupp6/svp/data/DataTransferObjects/DesignerData.java
@@ -0,0 +1,7 @@
+package grupp6.svp.data.DataTransferObjects;
+
+/**
+ * Created by alo on 2017-05-08.
+ */
+public class DesignerData extends DataTransferObject  {
+}
