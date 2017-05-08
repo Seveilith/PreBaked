febc5017576ba84cf52812d5bd041a7357015ccf
wagnborg <malinwagnborg@gmail.com>
2017-05-08 16:02:29 +0200

    försökte pa koppla form och sevrltregf

--- src/grupp6/svp/domain/Basket.java
+++ src/grupp6/svp/domain/Basket.java
@@ -1,8 +1,18 @@
 package grupp6.svp.domain;
-
+import java.util.HashMap;
 /**
  * Created by malinvagnborg on 2017-05-08.
  */
 public class Basket {
 
+    HashMap<String, Integer> basketItems;
+    public Basket(){
+    }
+    public HashMap getBasketItems(){
+        return basketItems;
+    }
+    public void addToBasket(String itemId, int price){
+        basketItems.put(itemId, price);
+    }
 }
+
