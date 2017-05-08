de9f311c6489fb76b9af1a347ec9e7cbbe63f8a1
wagnborg <malinwagnborg@gmail.com>
2017-05-08 13:29:29 +0200

    Lade till variabler

--- src/grupp6/svp/domain/Product.java
+++ src/grupp6/svp/domain/Product.java
@@ -4,4 +4,35 @@ package grupp6.svp.domain;
  * Created by malinvagnborg on 2017-05-08.
  */
 public class Product {
+
+
+    private int productId;
+
+    private String productName;
+
+    private String productDescription;
+
+    private int productPrice;
+
+    private int productQuantity;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
 }
