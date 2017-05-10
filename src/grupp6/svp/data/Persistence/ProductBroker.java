88d9f11a348edbbc60f89aa9f59909fda22f5c16
linnpetterssonn <linnlkp@telia.com>
2017-05-10 16:05:42 +0200

    :))

--- src/grupp6/svp/data/Persistence/ProductBroker.java
+++ src/grupp6/svp/data/Persistence/ProductBroker.java
@@ -1,7 +1,10 @@
 package grupp6.svp.data.Persistence;
 
 import grupp6.svp.data.DataTransferObjects.DataTransferObject;
+import grupp6.svp.data.DataTransferObjects.ProductData;
 import grupp6.svp.data.DbConnect;
+import sun.security.util.ObjectIdentifier;
+import grupp6.svp.domain.*;
 
 import java.sql.*;
 import java.util.UUID;
@@ -27,7 +30,36 @@ public class ProductBroker extends Broker {
     }
 
     @Override
-    public Object getFromStorage(UUID id) {
-        return null;
+    public Object getFromStorage(int id, Connection con) {
+        ProductData productData = new ProductData();
+
+        String sqlSelect = "SELECT * FROM pgiei02.Product WHERE ProductID = ?";
+
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sqlSelect);
+            preparedStatement.setObject(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()){
+                productData.setProductId(rs.getInt("ProductID"));
+                productData.setProductName(rs.getString("ProductName"));
+                productData.setProductDescription(rs.getString("ProductDescription"));
+                productData.setProductPrice(rs.getInt("ProductPrice"));
+                productData.setProductQuantity(rs.getInt("ProductQuantity"));
+
+                System.out.println(productData.getProductDescription());
+            }
+
+            dbCon.returnConnection(con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productData;
+    }
+
+    public static void main(String[] args){
+        ProductBroker broker = new ProductBroker();
+
+        broker.find(1);
     }
 }
