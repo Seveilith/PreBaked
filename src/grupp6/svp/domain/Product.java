a401d1a37c301d70c4c7484b6c3a79edde9e6dc0
Carl <carl.brannstrom@hotmail.com>
2017-05-10 14:16:54 +0200

    Merge branch 'master' of /Users/carlbrannstrom/IdeaProjects/PreBaked with conflicts.

--- src/grupp6/svp/domain/Product.java
+++ src/grupp6/svp/domain/Product.java
@@ -6,33 +6,4 @@ package grupp6.svp.domain;
 public class Product {
 
 
-    private int productId;
-
-    private String productName;
-
-    private String productDescription;
-
-    private int productPrice;
-
-    private int productQuantity;
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public int getProductPrice() {
-        return productPrice;
-    }
-
-    public int getProductQuantity() {
-        return productQuantity;
-    }
 }
